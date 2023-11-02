d708d182cf5c1ac83353ad0d282dbb13b70b8bcb
Will-Alger <algerw@icloud.com>
2023-11-02 16:24:16 -0400

    Improve profile page styling

--- resources/views/professor/show.blade.php
+++ resources/views/professor/show.blade.php
@@ -3,10 +3,13 @@
         <div class="bg-white shadow overflow-hidden sm:rounded-lg">
             <!-- Profile Header -->
             <div class="bg-white px-4 py-5 sm:px-6">
-                <h3 class="text-lg leading-6 font-bold">
+                <h2 class="text-xl leading-6 font-bold">
                     {{ $professor->firstName }} {{ $professor->lastName }}
-                </h3>
-                <p class="mt-1 text-sm">{{ $professor->department }}</p>
+                </h2>
+                <h4 class="text-sm leading-6 font-medium text-gray-500">
+                    {{ $professor->schoolName }}
+                    <p class="mt-1 text-sm">{{ 'Department: ' . $professor->department }}</p>
+                </h4>
             </div>
 
             <!-- Profile Body -->
@@ -20,15 +23,40 @@
                             </dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">
                                 <div class="w-1/4 bg-gray-200 rounded">
-                                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded"
+                                    <div class="bg-electric-violet text-xs font-medium text-blue-100 text-center p-1 leading-none rounded"
                                          style="width: {{ $professor->avgRating / 5 * 100 }}%; height: 20px;"></div>
                                 </div>
                                 <div class="text-sm font-medium text-gray-600 mt-2">
                                     {{ number_format($professor->avgRating, 1) }} / 5
                                 </div>
                             </dd>
+
+                            <dt class="text-sm font-medium text-gray-500">
+                                Average Difficulty
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">
+                                <div class="w-1/4 bg-gray-200 rounded">
+                                    <div class="bg-electric-violet text-xs font-medium text-blue-100 text-center p-1 leading-none rounded"
+                                         style="width: {{ $professor->avgDifficulty / 5 * 100 }}%; height: 20px;"></div>
+                                </div>
+                                <div class="text-sm font-medium text-gray-600 mt-2">
+                                    {{ number_format($professor->avgDifficulty, 1) }} / 5
+                                </div>
+                            </dd>
+
+                            <dt class="text-sm font-medium text-gray-500">
+                                Would Take Again
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">
+                                <div class="w-1/4 bg-gray-200 rounded">
+                                    <div class="bg-electric-violet text-xs font-medium text-blue-100 text-center p-1 leading-none rounded"
+                                         style="width: {{ $professor->wouldTakeAgainPercent }}%; height: 20px;"></div>
+                                </div>
+                                <div class="text-sm font-medium text-gray-600 mt-2">
+                                    {{ number_format($professor->wouldTakeAgainPercent, 0) }}%
+                                </div>
+                            </dd>
                         </div>
-                        <!-- Additional fields with progress bars as needed -->
                     </dl>
                 </div>
             </div>
