89e4b2a52ac72e500fc2ce737251fb768ca55c93
Will-Alger <algerw@icloud.com>
2023-11-16 13:52:31 -0500

    added example chart

--- database/migrations/2023_10_28_203454_create_schools_table.php
+++ database/migrations/2023_10_28_203454_create_schools_table.php
@@ -11,25 +11,27 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('schools', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->integer('legacyId')->nullable();
-            $table->string('name');
-            $table->integer('numRatings');
-            $table->string('state');
-            $table->float('campusCondition');
-            $table->float('campusLocation');
-            $table->float('careerOpportunities');
-            $table->float('clubAndEventActivities');
-            $table->float('foodQuality');
-            $table->float('internetSpeed');
-            $table->float('libraryCondition');
-            $table->float('schoolReputation');
-            $table->float('schoolSafety');
-            $table->float('schoolSatisfaction');
-            $table->float('socialActivities');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('schools')) {
+            Schema::create('schools', function (Blueprint $table) {
+                $table->string('id')->primary();
+                $table->integer('legacyId')->nullable();
+                $table->string('name');
+                $table->integer('numRatings');
+                $table->string('state');
+                $table->float('campusCondition');
+                $table->float('campusLocation');
+                $table->float('careerOpportunities');
+                $table->float('clubAndEventActivities');
+                $table->float('foodQuality');
+                $table->float('internetSpeed');
+                $table->float('libraryCondition');
+                $table->float('schoolReputation');
+                $table->float('schoolSafety');
+                $table->float('schoolSatisfaction');
+                $table->float('socialActivities');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
