c4d662ba000b6db3796f1c132c3d426f2afbda2a
Will-Alger <algerw@icloud.com>
2023-11-18 14:34:59 -0500

    added index to professor names

--- resources/views/user_search.blade.php
+++ resources/views/user_search.blade.php
@@ -11,24 +11,3 @@
         </div>
     </div>
 </x-app-layout>
-
-<script>
-    // document.addEventListener('selection', (event) => {
-    //     console.log("selection event heard");
-    // });
-    // make this its own card
-    // document.addEventListener('selectProfessor', (event) => {
-    //     let professor = event.detail;
-    //     document.getElementById('selectedProfessor').innerHTML =
-    //         `<div class="mt-5 p-6 w-full mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-    //         <div>
-    //             <div class="text-xl font-medium text-black">Professor: ${professor.firstName} ${professor.lastName}</div>
-    //             <p class="text-gray-500">School: ${professor.schoolName}</p>
-    //             <p class="text-gray-500">Department: ${professor.department}</p>
-    //             <p class="text-gray-500">Ratings: ${professor.numRatings}</p>
-    //             <p class="text-gray-500">Average Rating: ${professor.avgRating}</p>
-    //             <p class="text-gray-500">Would Take Again: ${professor.wouldTakeAgainPercent}%</p>
-    //         </div>
-    //     </div>`;
-    // });
-</script>
