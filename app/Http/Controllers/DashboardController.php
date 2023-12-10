c9eb64e8dd8f92027e940f5b69dd5659f51dba03
Will-Alger <algerw@icloud.com>
2023-12-10 18:09:51 -0500

    improve professor profile

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,52 +12,29 @@ use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
+
     public function index()
     {
-        $schoolAverages = Cache::remember('university_school_quality_charts', Carbon::now()->addMonth(), function () {
-            $schools = School::where('state', 'KY')
-                ->orderBy('numRatings', 'desc')
-                ->limit(6)
-                ->get();
-            return new QualityTrend($schools, getColors());
-        });
-
-        $computerScienceAverages = Cache::remember('computer_science_quality_charts', Carbon::now()->addMonth(), function () {
-            $schools = School::where('state', 'KY')
-                ->orderBy('numRatings', 'desc')
-                ->limit(6)
-                ->get();
-            return new QualityTrend($schools, getColors(), "Computer Science");
-        });
-
-        $englishAverages = Cache::remember('english_quality_charts', Carbon::now()->addMonth(), function () {
-            $schools = School::where('state', 'KY')
-                ->orderBy('numRatings', 'desc')
-                ->limit(6)
-                ->get();
-            return new QualityTrend($schools, getColors(), "English");
-        });
-
-        $mathematicsAverages = Cache::remember('mathematics_quality_charts', Carbon::now()->addMonth(), function () {
-            $schools = School::where('state', 'KY')
-                ->orderBy('numRatings', 'desc')
-                ->limit(6)
-                ->get();
-            return new QualityTrend($schools, getColors(), "Mathematics");
-        });
-
-        $historyAverages = Cache::remember('history_quality_charts', Carbon::now()->addMonth(), function () {
-            $schools = School::where('state', 'KY')
-                ->orderBy('numRatings', 'desc')
-                ->limit(6)
-                ->get();
-            return new QualityTrend($schools, getColors(), "History");
-        });
-
+        $schoolAverages = getQualityTrend();
+        $computerScienceAverages = getQualityTrend("Computer Science");
+        $englishAverages = getQualityTrend("English");
+        $mathematicsAverages = getQualityTrend("Mathematics");
+        $historyAverages = getQualityTrend("History");
         return view('dashboard', compact('schoolAverages', 'computerScienceAverages', 'englishAverages', 'mathematicsAverages', 'historyAverages'));
     }
 }
 function getColors()
 {
     return ['#6871FA', '#F80053', '#FEA25B', '#AB64FA', '#00CE96', '#17D3F4'];
+}
+
+function getQualityTrend($subject = null)
+{
+    return Cache::remember($subject ? $subject . '_quality_charts' : 'university_school_quality_charts', Carbon::now()->addMonth(), function () use ($subject) {
+        $schools = School::where('state', 'KY')
+            ->orderBy('numRatings', 'desc')
+            ->limit(6)
+            ->get();
+        return new QualityTrend($schools, getColors(), $subject);
+    });
 }
